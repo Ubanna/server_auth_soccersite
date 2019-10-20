8f4478c5bf1df602821f4d77b0edaa4af0c60e0c
Ubanna <danekehu@gmail.com>
2019-10-20 15:15:14 +0100

    fixing delete error

--- routes/delete.js
+++ routes/delete.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const User = require('../models/User');
 const mongoose = require("mongoose")
 
+router.get('/:userId', async (req, res)=>{
+  try{
+  const user = await User.findById(req.params.userId)
+  res.json(user)
+  } catch (err){
+    res.json({message:err})
+  }
+  })
+
 
 router.delete('/:userId', async function (req, res) {
     try {
